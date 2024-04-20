ded2b9190ad6f24c3a80f3a92ca8f4f91b368b0d
edelvarden <edelvarden@gmail.com>
2024-04-20 08:30:09 -0700

    prevent open program window when open context menu on right click

--- ColorPicker/Behaviors/NotifyIconBehavior.cs
+++ ColorPicker/Behaviors/NotifyIconBehavior.cs
@@ -35,7 +35,14 @@ namespace ColorPicker.Behaviors
             _notifyIcon.ContextMenu.MenuItems.Add(new MenuItem("Settings", OnSettingsClick) { ShowShortcut = false });
             _notifyIcon.ContextMenu.MenuItems.Add(new MenuItem("Close", onCloseClick) { ShowShortcut = false });
 
-            _notifyIcon.MouseClick += (s, e) => _settingsWindowHelper.ShowSettings();
+            _notifyIcon.MouseClick += (s, e) =>
+            {
+                // Prevent open program window when open context menu on right click
+                if (e.Button == MouseButtons.Left)
+                {
+                    _settingsWindowHelper.ShowSettings();
+                }
+            };
         }
         private void Current_Exit(object sender, ExitEventArgs e)
         {
